949aff0ffbcfaad2c7837eb6cf9aafff01a60903
Renan GEHAN <rgehan94@gmail.com>
2017-05-25 15:55:52 +0100

    Add App container and renders it

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("Ok");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './containers/App.jsx';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
