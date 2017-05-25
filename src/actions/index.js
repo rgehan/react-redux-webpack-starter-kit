1e6bba8aedf8de9798160df8f2c75dd6be926f53
Renan GEHAN <rgehan94@gmail.com>
2017-05-25 16:22:46 +0100

    Add demo action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,10 +3,13 @@ import * as types from '../constants/actions-types';
 const ActionCreators = {
   // Action creators go here
 
-  // someAction: val => ({
-  //   type: types.SOME_ACTION,
-  //   val: val,
-  // }),
+  incrementCounter: () => ({
+    type: types.INCREMENT_COUNTER,
+  }),
+
+  decrementCounter: () => ({
+    type: types.DECREMENT_COUNTER,
+  }),
 };
 
 export default ActionCreators;
