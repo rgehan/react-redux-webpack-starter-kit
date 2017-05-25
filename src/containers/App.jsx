8a9926561fa42534f5f8268ad21726811aa7c9bf
Renan GEHAN <rgehan94@gmail.com>
2017-05-25 16:30:56 +0100

    Connect container to redux

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-export default class App extends Component {
+import ActionCreators from '../actions';
+import Counter from '../components/Counter.jsx';
+
+class App extends Component {
   constructor() {
     super();
   }
@@ -9,7 +14,20 @@ export default class App extends Component {
     return (
       <div>
         <h1>react-redux-starter-kit</h1>
+        <Counter
+          {...this.props}
+        />
       </div>
     );
   }
 }
+
+const mapStateToProps = state => ({
+  counter: state.counter,
+});
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators(ActionCreators, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
