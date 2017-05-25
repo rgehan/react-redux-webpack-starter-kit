595aadd4ff87d7c1ceff0ac41e79d1db7b75f243
Renan GEHAN <rgehan94@gmail.com>
2017-05-25 16:18:32 +0100

    Create redux store and wraps the app with the provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
+import reducer from './reducers';
 import App from './containers/App.jsx';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+// Creates the redux store
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
+);
