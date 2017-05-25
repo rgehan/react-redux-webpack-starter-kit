b7b3bdf45de83fd737540359f464afb627bb16bf
Renan GEHAN <rgehan94@gmail.com>
2017-05-25 16:17:38 +0100

    Add action creators and action types constants

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+import * as types from '../constants/actions-types';
+
+const ActionCreators = {
+  // Action creators go here
+
+  // someAction: val => ({
+  //   type: types.SOME_ACTION,
+  //   val: val,
+  // }),
+};
+
+export default ActionCreators;
---
+++ src/constants/actions-types.js
@@ -0,0 +1,4 @@
+// Action types constants go here
+
+export const INCREMENT_COUNTER = "INCREMENT_COUNTER";
+export const DECREMENT_COUNTER = "DECREMENT_COUNTER";
